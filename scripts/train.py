cafd23cb610332e1fc4b86661532eb1cc0be1c81
Thomas-95IS <thomas.stokes@informed.com>
2019-04-28 13:32:13 +0100

    Add pickled model to training output.

--- scripts/train.py
+++ scripts/train.py
@@ -1,21 +1,17 @@
+import pickle
+
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import SGDClassifier
 
 
-mnist_df = pd.read_csv('data/train.csv')
-
-print(mnist_df.shape)
-
-mnist_df = mnist_df.dropna()
-
-print(mnist_df.shape)
-
-# mnist_labels = mnist_df['label']
-# mnist_values = mnist_df[mnist_df.columns.tolist()[1:]]
-
-# s = mnist_df['pixel4'].isna().sum()
-# print (s)
+mnist_df = pd.read_csv('data/cleaned_train.csv')
 
 sgd_clf = SGDClassifier(random_state=42)
 sgd_clf.fit(mnist_df[mnist_df.columns.tolist()[1:]], mnist_df['label'])
+
+
+with open('data/model.pkl', 'wb') as model_file:
+    pickle.dump(sgd_clf, model_file)
+
+model_file.close()
